92437593852a0368de53eb1b0c85cd8714f2e4e8
Huda Ansari <huda@nerve.solutions>
2026-08-25 11:42:40 +0530

    delete all

--- android/app/src/main/kotlin/com/example/century_cqg/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.century_cqg
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/Screens/Account/accnts_tabs/model/analysis_model.dart
+++
@@ -1,6 +0,0 @@
-class AnalysisMetric {
-  final String label;
-  final String value;
-
-  const AnalysisMetric({required this.label, required this.value});
-}
